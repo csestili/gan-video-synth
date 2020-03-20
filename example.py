261ef09028085186df0e0f0c8ac10864934869fc
Carson Sestili <sestilic@gmail.com>
2020-03-19 22:40:52 -0700

    Refactor into a questionably cleaner class.

--- example.py
+++ example.py
@@ -15,38 +15,88 @@ from datetime import datetime
 os.environ["TFHUB_CACHE_DIR"] = '/tmp/tfhub'
 
 
-# Load model
-# TODO allow model selection as flag
-# TODO save model after first download and load from there
-module_path = 'https://tfhub.dev/deepmind/biggan-deep-256/1'  # 256x256 BigGAN-deep
+class GanVideoSynth(object):
 
-tf.compat.v1.reset_default_graph()
-print('Loading BigGAN module from:', module_path)
-module = hub.Module(module_path)
-inputs = {k: tf.compat.v1.placeholder(v.dtype, v.get_shape().as_list(), k)
-          for k, v in module.get_input_info_dict().items()}
-output = module(inputs)
+  @property
+  def _module(self):
+    if self.__module is None:
+      # TODO allow model selection as flag
+      module_path = 'https://tfhub.dev/deepmind/biggan-deep-256/1'  # 256x256 BigGAN-deep
 
-print()
-print('Inputs:\n', '\n'.join(
-    '  {}: {}'.format(*kv) for kv in inputs.items()))
-print()
-print('Output:', output)
+      tf.compat.v1.reset_default_graph()
+      print('Loading BigGAN module from:', module_path)
+      self.__module = hub.Module(module_path)
+    return self.__module
+
+  @property
+  def _inputs(self):
+    if self.__inputs is None:
+      self.__inputs = {k: tf.compat.v1.placeholder(v.dtype, v.get_shape().as_list(), k)
+                for k, v in self._module.get_input_info_dict().items()}
+    return self.__inputs
+
+  @property
+  def _output(self):
+    if self.__output is None:
+      self.__output = self._module(self._inputs)
+    return self.__output
+
+  @property
+  def _session(self):
+    if self.__session is None:
+      # Create a TF session and initialize variables
+      initializer = tf.compat.v1.global_variables_initializer()
+      self.__session = tf.compat.v1.Session()
+      self.__session.run(initializer)
+    return self.__session
+
+  def __init__(self):
+    self.__module = None
+    self.__inputs = None
+    self.__output = None
+    self.__inputs = None
+    self.__session = None
+
+    self.input_z = self._inputs['z']
+    self.input_y = self._inputs['y']
+    self.input_trunc = self._inputs['truncation']
+
+    self.dim_z = self.input_z.shape.as_list()[1]
+    self.vocab_size = self.input_y.shape.as_list()[1]
+
+  def sample(self, noise, label, truncation=1., batch_size=8,
+             vocab_size=None):
+    if vocab_size is None:
+        vocab_size = self.vocab_size
+    noise = np.asarray(noise)
+    label = np.asarray(label)
+    num = noise.shape[0]
+    if len(label.shape) == 0:
+      label = np.asarray([label] * num)
+    if label.shape[0] != num:
+      raise ValueError('Got # noise samples ({}) != # label samples ({})'
+                       .format(noise.shape[0], label.shape[0]))
+    label = one_hot_if_needed(label, self.vocab_size)
+    ims = []
+    for batch_start in range(0, num, batch_size):
+      s = slice(batch_start, min(num, batch_start + batch_size))
+      feed_dict = {self.input_z: noise[s], self.input_y: label[s], self.input_trunc: truncation}
+      ims.append(self._session.run(self._output, feed_dict=feed_dict))
 
 
-input_z = inputs['z']
-input_y = inputs['y']
-input_trunc = inputs['truncation']
+    ims = np.concatenate(ims, axis=0)
+    assert ims.shape[0] == num
+    ims = np.clip(((ims + 1) / 2.0) * 256, 0, 255)
+    ims = np.uint8(ims)
+    return ims
 
-dim_z = input_z.shape.as_list()[1]
-vocab_size = input_y.shape.as_list()[1]
 
-def truncated_z_sample(batch_size, truncation=1., seed=None, dim=dim_z):
+def truncated_z_sample(batch_size, dim, truncation=1., seed=None):
   state = None if seed is None else np.random.RandomState(seed)
   values = truncnorm.rvs(-2, 2, size=(batch_size, dim), random_state=state)
   return truncation * values
 
-def one_hot(index, vocab_size=vocab_size):
+def one_hot(index, vocab_size):
   index = np.asarray(index)
   if len(index.shape) == 0:
     index = np.asarray([index])
@@ -56,36 +106,13 @@ def one_hot(index, vocab_size=vocab_size):
   output[np.arange(num), index] = 1
   return output
 
-def one_hot_if_needed(label, vocab_size=vocab_size):
+def one_hot_if_needed(label, vocab_size):
   label = np.asarray(label)
   if len(label.shape) <= 1:
     label = one_hot(label, vocab_size)
   assert len(label.shape) == 2
   return label
 
-def sample(sess, noise, label, truncation=1., batch_size=8,
-           vocab_size=vocab_size):
-  noise = np.asarray(noise)
-  label = np.asarray(label)
-  num = noise.shape[0]
-  if len(label.shape) == 0:
-    label = np.asarray([label] * num)
-  if label.shape[0] != num:
-    raise ValueError('Got # noise samples ({}) != # label samples ({})'
-                     .format(noise.shape[0], label.shape[0]))
-  label = one_hot_if_needed(label, vocab_size)
-  ims = []
-  for batch_start in range(0, num, batch_size):
-    s = slice(batch_start, min(num, batch_start + batch_size))
-    feed_dict = {input_z: noise[s], input_y: label[s], input_trunc: truncation}
-    ims.append(sess.run(output, feed_dict=feed_dict))
-
-
-  ims = np.concatenate(ims, axis=0)
-  assert ims.shape[0] == num
-  ims = np.clip(((ims + 1) / 2.0) * 256, 0, 255)
-  ims = np.uint8(ims)
-  return ims
 
 def interpolate(A, B, num_interps):
   if A.shape != B.shape:
@@ -133,14 +160,6 @@ def imshow(a, format='png', jpeg_fallback=True):
       raise
   return disp
 
-
-
-# Create a TF session and initialize variables
-initializer = tf.compat.v1.global_variables_initializer()
-sess = tf.compat.v1.Session()
-sess.run(initializer)
-
-
 def write_gif(ims, duration=4, fps=30, fname='ani.gif'):
 
   def make_frame(t):
@@ -154,6 +173,8 @@ def write_gif(ims, duration=4, fps=30, fname='ani.gif'):
     clip.write_gif(fname, fps=fps, verbose=False)
   elif fname.endswith('.mp4'):
     clip.write_videofile(fname, fps=fps, verbose=False, codec='mpeg4')
+
+
     
 
 
@@ -167,17 +188,17 @@ truncation = 1 #@param {type:"slider", min:0.02, max:1, step:0.02}
 duration = 1 #@param {type:"slider", min:1, max:10, step:0.5}
 num_samples = 1 #@param {type:"slider", min:1, max:100, step:1}
 
-def generate(fps=30):
+def generate(gan_video_synth, fps=30):
 
   num_interps = duration * fps
 
   # # Random label vec
-  # y = truncated_z_sample(1, truncation=truncation, seed=noise_seed_y, dim=vocab_size)
+  # y = truncated_z_sample(1, vocab_size, truncation=truncation, seed=noise_seed_y)
 
   # Indexed label vec
   y_axes = [309]
   y_magnitude = 0.9
-  y = np.zeros((1, vocab_size))
+  y = np.zeros((1, gan_video_synth.vocab_size))
   for axis in y_axes:
     y[0, axis] = 1
   y = y / np.linalg.norm(y) * y_magnitude
@@ -187,7 +208,7 @@ def generate(fps=30):
 
   # Random z vec
   noise_seed_z = int(datetime.now().strftime('%f'))
-  z0 = truncated_z_sample(1, truncation, noise_seed_z)
+  z0 = truncated_z_sample(1, gan_video_synth.dim_z, truncation, noise_seed_z)
 
   # Interpolation settings
   # Axes to change in [0, 128]
@@ -222,7 +243,7 @@ def generate(fps=30):
 
   # Generate images
   t0 = datetime.now()
-  ims = sample(sess, zs, ys, truncation=truncation)
+  ims = gan_video_synth.sample(zs, ys, truncation=truncation)
   t1 = datetime.now()
   elapsed = (t1 - t0).seconds + 10 ** -6 * (t1 - t0).microseconds
   print("{} sec to generate {} frames".format(elapsed, num_interps))
@@ -235,8 +256,10 @@ def generate(fps=30):
   elapsed = (t2 - t1).seconds + 10 ** -6 * (t2 - t1).microseconds
   print("{} sec to write gif".format(elapsed))
 
+gan_video_synth = GanVideoSynth()
+
 # TODO generate multiple samples as a batch, not as a loop
 for _ in range(num_samples):
-  generate()
+  generate(gan_video_synth)
 
 
